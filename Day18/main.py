29abba0c15cb7b0873acc1abf19fb4c585561c5e
Andrea <82764646+Dozzy85@users.noreply.github.com>
2022-10-01 19:47:13 +0200

    Update main.py

--- Day18/main.py
+++ Day18/main.py
@@ -1,6 +1,18 @@
 from turtle import Turtle, Screen
+import random
 
-timmy_the_turtle = Turtle()
+tim = Turtle()
 
-screen = Screen()
-screen.exitonclick()
+colors = ["MediumBlue", "LimeGreen", "DarkOrange", "Indigo", "SaddleBrown", "IndianRed", "SpringGreen", "Aquamarine"]
+
+def draw_shape(num_sides):
+    for _ in range(num_sides):
+        angole = 360 / num_sides
+        tim.forward(100)
+        tim.right(angole)
+
+for shape_side_n in range(3, 10):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+#LEZIONE 169 CHALLANGE 4 DA FARE
